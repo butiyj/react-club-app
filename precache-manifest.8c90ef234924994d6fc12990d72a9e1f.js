467cade169d5548729c31ac98a6289c268d08609
butiyj <buti0523@gmail.com>
2022-07-17 19:20:33 +0900

    Updates

---
+++ precache-manifest.8c90ef234924994d6fc12990d72a9e1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf23d7216062eac43615",
+    "url": "/react-club-app/static/css/main.6d2fe0a8.chunk.css"
+  },
+  {
+    "revision": "cf23d7216062eac43615",
+    "url": "/react-club-app/static/js/main.cf23d721.chunk.js"
+  },
+  {
+    "revision": "1a4d7d1271a2d9e8d8ce",
+    "url": "/react-club-app/static/js/1.1a4d7d12.chunk.js"
+  },
+  {
+    "revision": "56231a1dd1e60147089d",
+    "url": "/react-club-app/static/js/runtime~main.56231a1d.js"
+  },
+  {
+    "revision": "b352775ba741f2d236f3b7067ab38be3",
+    "url": "/react-club-app/index.html"
+  }
+];
